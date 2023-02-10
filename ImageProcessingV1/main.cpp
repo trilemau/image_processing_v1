19196046790f38a18b9f8c81ad34a185f5954269
trilemau <tirlemau@gmail.com>
2023-02-10 20:05:17 +0100

    comparison changes

--- ImageProcessingV1/main.cpp
+++ ImageProcessingV1/main.cpp
@@ -2,7 +2,7 @@
 
 //#define IMAGE_PATH "C:/Users/stefq/Pictures/batman.jpg"
 #define IMAGE_PATH "C:/Users/stefq/Pictures/314383957_1295843501270009_3371358039605884926_n.jpg"
-#define ITERATIONS 1
+#define ITERATIONS 20
 
 #include "ImageProcessingCpp.h"
 #include "ImageProcessingOpenCL.h"
@@ -10,7 +10,7 @@
 // TODO list
 // TODO PNG
 
-void processImage(IImageProcessing& image_processing)
+int processImage(IImageProcessing& image_processing)
 {
     auto start = std::chrono::system_clock::now();
 
@@ -29,9 +29,12 @@ void processImage(IImageProcessing& image_processing)
 
     auto difference = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-    std::cout << "------" << image_processing.Info() << "------\n";
+    std::cout << "\n------" << image_processing.Info() << "------\n";
     std::cout << "TotalTime=" << difference.count() << "ms\n";
     std::cout << "AverageTime=" << difference.count() / ITERATIONS << "s\n";
+    std::cout << "\n";
+
+    return difference.count();
 }
 
 void comparison()
@@ -40,8 +43,10 @@ void comparison()
     ImageProcessingOpenCL image_processing_opencl;
 
 
-    //processImage(image_processing_cpp);
-    processImage(image_processing_opencl);
+    auto time_cpp = processImage(image_processing_cpp);
+    auto time_opencl = processImage(image_processing_opencl);
+
+    std::cout << "multiple=" << time_cpp / time_opencl * 100 << "x\n";
 }
 
 void demo()
@@ -65,8 +70,8 @@ void demo()
 
 int main()
 {
-    //comparison();
-    demo();
+    comparison();
+    //demo();
 
     return 0;
 }
